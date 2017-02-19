dc8b88aeb2768cb4e47a5a46b68f3bb12d69aac4
mpokropowicz <pokropowicz.shinto+git@gmail.com>
2017-02-19 14:18:38 -0500

    test_change passes

--- change_maker.rb
+++ change_maker.rb
@@ -4,3 +4,43 @@ def parse_change(num)
 
 	num[num.index('.') + 1,num.length].to_i
 end
+
+def change(amount)
+
+	available_coins  = [25,10,5,1]
+    coins            = []
+    remaining_amount = amount
+
+    available_coins.each do |coin|
+
+    	if remaining_amount/coin > 0
+
+    		(remaining_amount/coin).times { coins << coin}
+
+    		remaining_amount = amount - coins.inject(:+)
+    	end
+    end
+
+    return coins_to_hash(coins)
+end
+
+def name_of_coin(coin)
+
+	case coin
+
+		when 25 then :quarter
+		when 10 then :dime
+		when 5 then :nickle
+		when 1 then :penny
+	end
+end
+
+def coins_to_hash(array)
+
+	array.each_with_index { |coin, index| array[index] = name_of_coin(coin)}
+
+	counts = Hash.new(0)
+	array.each { |name| counts[name] += 1}
+
+	return counts
+end
--- change_maker_test.rb
+++ change_maker_test.rb
@@ -3,17 +3,16 @@ require_relative "change_maker.rb"
 
 class TestChangeMaker < Minitest::Test 
 
-	@change_of_11 = {:dime => 1, :penny => 1}
+	$change_of_11 = {:dime => 1, :penny => 1}
 
 	def test_returns_change
 
 		assert_equal(11, parse_change(1.11))
 	end
 
-	def test_dimes_pennys
+	def test_change
 
-		results = correct_change(11)
-
-		assert_equal(@change_of_11, results)
+		results = change(11)
+		assert_equal($change_of_11, results)
 	end
 end

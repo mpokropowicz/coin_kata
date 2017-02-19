94aa280acba75afe3c751fd93946684257d152da
mpokropowicz <pokropowicz.shinto+git@gmail.com>
2017-02-19 13:09:48 -0500

    test_dimes_pennies fails

--- change_maker_test.rb
+++ change_maker_test.rb
@@ -3,8 +3,17 @@ require_relative "change_maker.rb"
 
 class TestChangeMaker < Minitest::Test 
 
+	@change_of_11 = {:dime => 1, :penny => 1}
+
 	def test_returns_change
 
 		assert_equal(11, parse_change(1.11))
+	end
+
+	def test_dimes_pennys
+
+		results = correct_change(11)
+
+		assert_equal(@change_of_11, results)
 	end
 end

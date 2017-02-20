213903ab669a91e666c6fa35e9264a890c50ecdd
mpokropowicz <pokropowicz.shinto+git@gmail.com>
2017-02-20 16:55:58 -0500

    refactored code heavily

--- change_maker.rb
+++ change_maker.rb
@@ -7,40 +7,18 @@ end
 
 def change(amount)
 
-	available_coins  = [25,10,5,1]
-    coins            = []
-    remaining_amount = amount
+	available_coins = {quarter: 25, dime: 10, nickel: 5, penny: 1}
+	coins = {}
+	remaining_amount = amount
 
-    available_coins.each do |coin|
+	available_coins.each do |coin, value|
 
-    	if remaining_amount/coin > 0
+		if remaining_amount/value > 0
 
-    		(remaining_amount/coin).times { coins << coin}
-
-    		remaining_amount = amount - coins.inject(:+)
+			coins[coin] = 0
+    		(remaining_amount/value).times{ coins[coin] += 1; remaining_amount -= value }
     	end
-    end
-
-    return coins_to_hash(coins)
-end
-
-def name_of_coin(coin)
-
-	case coin
-
-		when 25 then :quarter
-		when 10 then :dime
-		when 5 then :nickel
-		when 1 then :penny
 	end
-end
 
-def coins_to_hash(array)
-
-	array.each_with_index { |coin, index| array[index] = name_of_coin(coin)}
-
-	counts = Hash.new(0)
-	array.each { |name| counts[name] += 1}
-
-	return counts
+	coins
 end
